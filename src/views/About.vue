a482961efb8be2282914593ebc50a2636ce83d0f
AndreeaCCA <andreeaccandreea@gmail.com>
2021-04-07 21:52:59 +0200

    about prueba

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,25 +1,58 @@
-<template> 
-  <div>
-    <h1>About</h1>
-
+<template>
+  <div class="container-fluid justify-content-center">
+    <div class="col col-6 offset-3">
+      <h2>About Us</h2>
+      <div class="row text-white">
+        <div class="col-lg-10">
+          <div class="bg-white p-5 rounded my-5 shadow-sm">
+            <p class="lead font-italic mb-0 text-muted">
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+            </p>
+                
+          </div>
+          <div class="bg-white p-5 rounded my-5 shadow-sm">
+            <p class="lead font-italic mb-0 text-muted">
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+            </p>
+                
+          </div>
+          <div class="bg-white p-5 rounded my-5 shadow-sm">
+            <p class="lead font-italic mb-0 text-muted">
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+            </p>
+                
+          </div>
+          <div class="bg-white p-5 rounded my-5 shadow-sm">
+            <p class="lead font-italic mb-0 text-muted">
+              "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+            </p>
+                
+          </div>
+        </div>    
+      </div>
+  
+      <mdb-modal-footer class="mx-5 pt-3 mb-1"> </mdb-modal-footer>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
   name: "About",
-  components: {
-
-  },
+  components: {},
 
   setup() {
-    return {
-        
-    }
+    return {};
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
