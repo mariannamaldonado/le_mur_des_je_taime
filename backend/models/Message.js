4980146f0b6526e5476d80da992dbee288002ce4
mariannamaldonado <mariannamldndo@gmail.com>
2021-04-15 11:54:34 +0200

    page list message

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose
 
 const schemaMessage = new Schema({
     message: { type: String, required: false },
-    author: { type: String, required: false},
+    author: { type: String, required: false}, 
     encryption: { type: Boolean, default: false },
     encryption_key: { type: Boolean, default: false },
     properties: { type: String, default: "Times New Roman" },
@@ -11,7 +11,7 @@ const schemaMessage = new Schema({
     size: { type: String },
     receiver: { type: String, required: false },
     user: { type: Schema.Types.ObjectId, ref: 'User' }
-})
+}) 
 
 class Message {
     //set y get
