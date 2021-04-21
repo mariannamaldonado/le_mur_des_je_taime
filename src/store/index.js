f92504a86ebabbfd18dfc0f7deef6af4f87f6fde
Jeanpi <je5x.1.jnes@gmail.com>
2021-04-21 23:57:51 +0200

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -2,28 +2,31 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    user:{}
-  },
-  getters: {
-    getUser(state){
-      return state.user
-    }
+    user: {}
   },
   mutations: {
-    setUser(state,valor){
-      state.user=valor
-    },  
+    setUser(state, payload) {
+
+    }
   },
   actions: {
-    fetchUsers(mutations){
-      fetch('http://localhost:8081/api/users/listar')
-        .then(resp=>resp.json())
-        .then(datos=>{
-          mutations.commit('setUser',datos[0])
+    async login({ commit }, user) {
+      console.log(user)
+      try {
+        const res = await fetch('http://localhost:8081/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(user)
         })
+        const userDB = await res.json()
+          console.log(userDB)
+      } catch (error) {
+        console.log(error)
+      }
     }
   },
   modules: {
-    
   }
 })
